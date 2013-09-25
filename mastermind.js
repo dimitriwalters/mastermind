1e60ca98c48d2de36de066b14530301f64e03bf1
DimitriWalters <me@dimitriwalters.com>
2013-09-24 20:49:25 -0400

    Redevelop the app

--- mastermind.js
+++ mastermind.js
@@ -1,86 +1,81 @@
-function addGuesses() {
-   for (i=1; i<=12; i++) {
-   	$("#guessed").append("<div id='guess"+i+"'>#"+i+":</div>");
-   }
-}
-
-function resetGame () {
-	$("#guessInput").removeAttr("disabled");
-    $("#guessButton").removeAttr("disabled");
-	guesses = 0;
-	$('#guessed').text('');
-	$('#guessInput').val('');
-	code = "";
-	numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-	for (var i=0; i<4; i++) {
-		number = Math.floor(Math.random() * (9-i));
-		code += numbers[number];
-		numbers.splice(number, 1);
-	}
-	addGuesses();
-}
-
-function correctPos (guess, code) {
-	count = 0;
-	for (var i=0; i<guess.length; i++) {
-		if (guess.charAt(i) == code.charAt(i)) {
-			count++;
+function MastermindCntl($scope, $location) {
+	$scope.resetGame  = function () {
+		$scope.number = '';
+		$scope.done = false;
+		numberOfGuesses = 0;
+		secretCode = "";
+		numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+		for (var i=0; i<4; i++) {
+			number = Math.floor(Math.random() * (9-i));
+			secretCode += numbers[number];
+			numbers.splice(number, 1);
 		}
+		$scope.guesses = [];
+		for (var i=1; i<=$scope.limit; i++)
+			$scope.guesses.push({'code': '#'+i+':'});
 	}
-	return count;
-}
 
-function wrongPos (guess, code) {
-	count = 0;
-	for (var i=0; i<guess.length; i++) {
-		if ((guess.charAt(i) != code.charAt(i)) && (code.indexOf(guess.charAt(i)) != -1)) {
-			count++;
+	$scope.checkGuess = function () {
+		numberOfGuesses++;
+		var guess = $scope.number;
+		if (guess.length != 4) {
+			alert('Please enter a 4 digit number');
+			numberOfGuesses--;
 		}
+		else if (guess == secretCode) {
+			alert('You won!');
+			$scope.guesses[numberOfGuesses-1].code = '#'+numberOfGuesses+': '+guess+' : You won!';
+			numberOfGuesses--;
+			gameOver();
+		}
+		else {
+			c = correctPoints(guess, secretCode);
+			w = wrongPoints(guess, secretCode);
+			$scope.guesses[numberOfGuesses-1].code = '#'+numberOfGuesses+': '+guess+' : '+c+' correct, '+w+' wrong';
+		}
+		if (numberOfGuesses == $scope.limit) {
+			alert('You lost! The code was '+secretCode+'.');
+			gameOver();
+		}
+		$scope.number = '';
 	}
-	return count;
-}
 
-function gameOver () {
-	$("#guessInput").attr("disabled", "disabled");
-    $("#guessButton").attr("disabled", "disabled");
-}
+	$scope.showCode = function () {
+		alert('The code was: '+secretCode+'. A new game will start.');
+		$scope.resetGame();
+	}
 
-function checkGuess () {
-	guesses++;
-	guess = $('#guessInput').val();
-	if (guess.length != 4) {
-		alert('Please enter a 4 digit number');
-		guesses--;
+	$scope.validNumber = function () {
+		$scope.number = $scope.number.replace(/[^1-9\.]/g,'');
 	}
-	else if (guess == code) {
-		alert('You won!');
-		$('#guess'+guesses).text('#'+guesses+': '+guess+' : You won!');
-		guesses--;
-		gameOver();
-	}
-	else {
-		m = correctPos(guess, code);
-		n = wrongPos(guess, code);
-		$('#guess'+guesses).text('#'+guesses+': '+guess+' : '+m+' correct, '+n+' wrong');
-	}
-	if (guesses == 12) {
-		alert('You lost! The code was '+code);
-		gameOver();
-	}
-	$('#guessInput').val('');
-}
 
-function showCode () {
-	alert('The code was: '+code+'. A new game will start.');
-	resetGame();
-}
+	function init () {
+		var numberOfGuesses = 0;
+		var secretCode = "";
+		var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+		$scope.limit = 12;
+		$scope.resetGame();
+	}
 
-$('document').ready(function() {
-	resetGame();
-	$('#guessButton').click(checkGuess);
-	$('#codeButton').click(showCode);
-	$('#gameButton').click(resetGame);
-	jQuery('#guessInput').keyup(function () { 
-    this.value = this.value.replace(/[^1-9\.]/g,'');
-	});
-});
+	function correctPoints (guessedCode, actualCode) {
+		count = 0;
+		for (var i=0; i<guessedCode.length; i++)
+			if (guessedCode.charAt(i) == actualCode.charAt(i))
+				count++;
+		return count;
+	}
+
+	function wrongPoints (guessedCode, actualCode) {
+		count = 0;
+		for (var i=0; i<guessedCode.length; i++)
+			if ((guessedCode.charAt(i) != actualCode.charAt(i)) && (actualCode.indexOf(guessedCode.charAt(i)) != -1))
+				count++;
+		return count;
+	}
+
+	function gameOver () {
+		$scope.done = true;
+	}
+
+	init();
+}
